7c67b62432f1ebf07d80cfffd8e450678be98bb6
Oleg Fedorov <olegajaro94@gmail.com>
2022-03-19 10:23:59 +0300

    setup: Create new Xcode project without storyboards, add gitignore file

---
+++ PasswordResetScreen/PasswordResetScreen/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PasswordResetScreen
+//
+//  Created by Олег Федоров on 19.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
