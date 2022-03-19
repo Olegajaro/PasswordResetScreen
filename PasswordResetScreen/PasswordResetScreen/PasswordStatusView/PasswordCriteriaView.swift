1da183cccaea6c465d3195eac4f9392ea34e72f5
Oleg Fedorov <olegajaro94@gmail.com>
2022-03-19 17:17:47 +0300

    feat: Add password criteria view

--- PasswordResetScreen/PasswordResetScreen/PasswordStatusView/PasswordCriteriaView.swift
+++ PasswordResetScreen/PasswordResetScreen/PasswordStatusView/PasswordCriteriaView.swift
@@ -9,8 +9,37 @@ import UIKit
 
 class PasswordCriteriaView: UIView {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    let checkmarkImage = UIImage(systemName: "checkmark.circle")!.withTintColor(
+        .systemGreen,
+        renderingMode: .alwaysOriginal
+    )
+    let xmarkImage = UIImage(systemName: "xmark.circle")!.withTintColor(
+        .systemRed,
+        renderingMode: .alwaysOriginal
+    )
+    let circleImage = UIImage(systemName: "circle")!.withTintColor(
+        .tertiaryLabel,
+        renderingMode: .alwaysOriginal
+    )
+    
+    var isCriteriaMet: Bool = false {
+        didSet {
+            if isCriteriaMet {
+                imageView.image = checkmarkImage
+            } else {
+                imageView.image = xmarkImage
+            }
+        }
+    }
+    
+    init(text: String) {
+        super.init(frame: .zero)
+        
+        label.text = text
         
         style()
         layout()
@@ -23,16 +52,53 @@ class PasswordCriteriaView: UIView {
     override var intrinsicContentSize: CGSize {
         CGSize(width: 200, height: 40)
     }
+    
+    func reset() {
+        isCriteriaMet = false
+        imageView.image = circleImage
+    }
 }
 
 extension PasswordCriteriaView {
     
     func style() {
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .systemOrange
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 8
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "circle")! .withTintColor(
+            .tertiaryLabel,
+            renderingMode: .alwaysOriginal
+        )
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        label.textColor = .secondaryLabel
     }
     
     func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
         
+        addSubview(stackView)
+        
+        // stackView constraints
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        
+        // imageView constraints
+        NSLayoutConstraint.activate([
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+        ])
+        
+        // CHCR
+        imageView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        label.setContentHuggingPriority(.defaultLow, for: .horizontal)
     }
 }
--- PasswordResetScreen/PasswordResetScreen/ViewController.swift
+++ PasswordResetScreen/PasswordResetScreen/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     let stackView = UIStackView()
     let newPasswordTextField = PasswordTextField(placeholderText: "New password")
-    let criteriaView = PasswordCriteriaView()
+    let criteriaView = PasswordCriteriaView(text: "uppercase letter (A-Z)")
     
     override func viewDidLoad() {
         super.viewDidLoad()
